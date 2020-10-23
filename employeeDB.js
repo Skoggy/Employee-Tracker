c1b8a626928cf0a50fdecfa65faf99e438fe2206
Chris Skogstad <chris.skogstad1@gmail.com>
2020-10-23 23:35:10 +1100

    have basic inquirer set up to allow main question to be asked and then goes into switch to handle the answer

--- employeeDB.js
+++ employeeDB.js
@@ -19,5 +19,52 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err, res) {
     if (err) throw err;
-    console.log(`Server working ${res}`);
+    askWhatWant();
 });
+
+
+function askWhatWant() {
+    inquirer
+        .prompt({
+            name: "name",
+            type: "list",
+            message: "What would you like to do: ",
+            choices: [
+                "View all employees",
+                "View all departments",
+                "View all roles",
+                "Add employee",
+                "Add department",
+                "Add role",
+                "Update employee role"
+            ]
+        }).then(function (answer) {
+            switch (answer.action) {
+                case "View all employees":
+                    console.log("View all employees")
+                    break;
+
+                case "View all departments":
+                    console.log("View all departments")
+                    break;
+
+                case "View all roles":
+                    console.log("View all roles")
+                    break;
+
+                case "Add employee":
+                    console.log("Add employee")
+                    break;
+
+                case "Add role":
+                    console.log("Add role")
+                    break;
+
+                case "Update employee role":
+                    console.log("Update employee role")
+                    break;
+
+
+            }
+        })
+}
