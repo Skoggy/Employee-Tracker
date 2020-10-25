39d54fed057a59c4e571057053f0f7404065e4f7
Chris Skogstad <chris.skogstad1@gmail.com>
2020-10-25 13:59:46 +1100

    can now by choice decide how you want to organize the full employee list, by first name, last name, department, role and salary

--- employeeDB.js
+++ employeeDB.js
@@ -34,8 +34,6 @@ function askWhatWant() {
             message: "What would you like to do: ",
             choices: [
                 "View all employees",
-                "View all departments",
-                "View all roles",
                 "Add employee",
                 "Add department",
                 "Add role",
@@ -70,22 +68,41 @@ function askWhatWant() {
         })
 }
 function viewEmployees() {
+    inquirer
+        .prompt({
+            name: "choice",
+            type: "list",
+            message: "How would you like to view employees",
+            choices: [
+                "First Name",
+                "Last Name",
+                "Department",
+                "Role",
+                "Manager"
+            ]
+        }).then(function (answer) {
 
-    var query = 'SELECT e.id, e.first_name AS "First Name", e.last_name AS "Last Name", IFNULL(r.title, "No Data") AS "Title", IFNULL(d.name, "No Data") AS "Department", IFNULL(r.salary, "No Data") AS "Salary", CONCAT(m.first_name," ",m.last_name) AS "Manager" FROM employee e LEFT JOIN role r ON r.id = e.role_id LEFT JOIN department d ON d.id = r.department_id LEFT JOIN employee m ON m.id = e.manager_id ORDER BY e.id';
+            let choiceObj = {
+                "First Name": "e.first_name",
+                "Last Name": "e.last_name",
+                "Department": "d.name",
+                "Role": "r.title",
+                "Manager": "e.manager_id"
+            }
+            let queryParam = choiceObj[`${answer.choice}`]
 
+            var query = 'SELECT e.id, e.first_name AS "First Name", e.last_name AS "Last Name", IFNULL(r.title, "No Data") AS "Title", IFNULL(d.name, "No Data") AS "Department", IFNULL(r.salary, "No Data") AS "Salary", CONCAT(m.first_name," ",m.last_name) AS "Manager" FROM employee e LEFT JOIN role r ON r.id = e.role_id LEFT JOIN department d ON d.id = r.department_id LEFT JOIN employee m ON m.id = e.manager_id ORDER BY ' + queryParam;
 
-
-
-
-
-
-
-    connection.query(query, function (err, res) {
-        console.log("\n")
-        console.table(res)
-        askWhatWant()
-    })
+            connection.query(query, function (err, res) {
+                console.log("\n")
+                console.table(res)
+                askWhatWant()
+            })
+        })
 }
+
+
+
 figlet('Employee Tracker', function (err, data) {
     if (err) {
         console.log('Something went wrong...');
